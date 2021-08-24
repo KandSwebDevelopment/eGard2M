c94f033114e5b17583b845f1b9855a35c4966077
KandSwebDevelopment <github@kands.me.uk>
2021-08-24 22:00:38 +0100

    Build 1.0.53

--- class_output_workshop_heater.py
+++ class_output_workshop_heater.py
@@ -37,7 +37,7 @@ class OutputWorkshopHeater(OutputClass):
         else:
             self.boost_timer.stop()
             self.remaining = 0
-
+            getattr(self.output_controller.main_panel, "lbl_output_number_%i" % self.ctrl_id).setText("")
 
     def load_settings(self):
         self.auto_boost = int(self.db.get_config(CFT_WORKSHOP_HEATER, "auto boost", 1))
